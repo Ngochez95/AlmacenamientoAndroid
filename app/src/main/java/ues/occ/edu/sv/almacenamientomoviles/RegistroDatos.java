d2a6d21bf7973a8cfbceb72f07d86804a8e21770
Ngochez95 <nhgochez95@gmail.com>
2020-05-29 13:08:14 -0600

    Interfaz para almacenamiento de registros públicos y privados

---
+++ app/src/main/java/ues/occ/edu/sv/almacenamientomoviles/RegistroDatos.java
@@ -0,0 +1,20 @@
+package ues.occ.edu.sv.almacenamientomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RegistroDatos extends AppCompatActivity {
+
+    private TextView txtTitulor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_datos);
+        Bundle registro = this.getIntent().getExtras();
+        txtTitulor=(TextView) findViewById(R.id.txtTituloRegistro);
+        txtTitulor.setText(registro.getString("titulo"));
+    }
+}
